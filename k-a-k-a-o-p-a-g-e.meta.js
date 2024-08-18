186ce865cfdaea850c39a26aaa341f9af8eed02b
ogawaren3456 <ogawaren3456@github.com>
2024-08-18 21:13:41 +0700

    up

---
+++ k-a-k-a-o-p-a-g-e.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         k-a-k-a-o-p-a-g-e
+// @namespace    https://github.com/ogawaren3456/chay_xa_bay/raw/main/k-a-k-a-o-p-a-g-e.user.js
+// @version      2024.08.18
+// @description  down k-a-k-a-o-p-a-g-e
+// @author       You
+// @include      /^https:\/\/p[a@]ge\.k[a@]k[a@]o\.c[o0]m\/c[o0]nt[e3]nt\/.+?\/vi[e3]wer\//
+// @icon         https://raw.githubusercontent.com/ogawaren3456/chay_xa_bay/main/k-a-k-a-o-p-a-g-e.png
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/ogawaren3456/chay_xa_bay/raw/main/k-a-k-a-o-p-a-g-e.user.js
+// @updateURL    https://github.com/ogawaren3456/chay_xa_bay/raw/main/k-a-k-a-o-p-a-g-e.meta.js
+// ==/UserScript==

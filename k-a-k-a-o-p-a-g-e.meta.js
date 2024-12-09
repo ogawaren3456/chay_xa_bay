77d41870724e9f5386da6b34e0dfbe56f7fbb6f1
ogawaren3456 <ogawaren3456@github.com>
2024-12-09 07:46:46 +0700

    up

--- k-a-k-a-o-p-a-g-e.meta.js
+++ k-a-k-a-o-p-a-g-e.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         k-a-k-a-o-p-a-g-e
 // @namespace    https://github.com/ogawaren3456/chay_xa_bay/raw/main/k-a-k-a-o-p-a-g-e.user.js
-// @version      2024.08.18
+// @version      2024.12.09
 // @description  down k-a-k-a-o-p-a-g-e
 // @author       You
 // @include      /^https:\/\/p[a@]ge\.k[a@]k[a@]o\.c[o0]m\/c[o0]nt[e3]nt\/.+?\/vi[e3]wer\//
 // @icon         https://raw.githubusercontent.com/ogawaren3456/chay_xa_bay/main/k-a-k-a-o-p-a-g-e.png
-// @grant        GM_download
-// @grant        GM_info
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
 // @downloadURL  https://github.com/ogawaren3456/chay_xa_bay/raw/main/k-a-k-a-o-p-a-g-e.user.js
 // @updateURL    https://github.com/ogawaren3456/chay_xa_bay/raw/main/k-a-k-a-o-p-a-g-e.meta.js
 // ==/UserScript==
